a1caa11bd1825784910454a83adc9cd30151e56d
Vladislav <90910484+vladislav281998@users.noreply.github.com>
2024-04-21 22:10:31 +0200

    Added files for 05/ex2

---
+++ 05/ex2/ex2_flush.c
@@ -0,0 +1,36 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+
+    int data;
+    int flag = 0;
+
+    #pragma omp parallel num_threads(2)
+    {
+      if (omp_get_thread_num() == 0) {
+
+          data = 42;
+          
+          flag = 1;
+          #pragma omp flush(flag)
+
+      } else if(omp_get_thread_num() == 1) {
+        
+          int flag_val = 0;
+
+          while (flag_val < 1) {
+            
+             #pragma omp flush(flag)
+             flag_val = flag;
+
+          }
+
+          printf("flag=%d data=%d\n", flag, data);
+
+          printf("flag=%d data=%d\n", flag, data);
+
+      }
+    }
+    return 0;
+}
